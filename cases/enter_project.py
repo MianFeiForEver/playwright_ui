ffd26961955ddf1a176c37a827ff7d5f78b0dc98
liyanqing <liyanqing@jwzg.com>
2023-09-05 15:17:24 +0800

    测试

--- cases/enter_project.py
+++ cases/enter_project.py
@@ -15,5 +15,22 @@ def test_login_success(page: Page):
 
 
 def test_run(page: Page):
+    session = page.context.new_cdp_session(page)
+    session.send("Overlay.setShowFPSCounter", {"show": True})
+
+    session.send('Performance.enable')
+    login = Login(page)
+    login.login_by_password("15011290391", "12345678")
+
     project = Project(page)
-    project.zoom(10)
+    project.zoom(2)
+    report = session.send("Performance.getMetrics").get("metrics")
+    report_dict = {}
+    for item in report:
+        report_dict[item["name"]] = item["value"]
+    tti = report_dict["NavigationStart"] + report_dict["ProcessTime"]
+    ctti = report_dict["DomContentLoaded"] + report_dict["ProcessTime"]
+    print("tti", round(tti / 1000, 2))
+    print("ctti", round(ctti / 1000, 2))
+
+    # run_lighthouse(page.url)
